ae5ce2fdee466368897b3c2ba83a70fe9d318de6
John Doe <you@example.com>
2021-09-14 13:13:17 +0700

    menambahkan komentar

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,5 +1,10 @@
+/*
+Nama: Salma Awliya Nafsani
+NIM: 21537144015
+*/
 public class Hello {
     public static void main(String[] args) {
+        //untuk memunculkan teks
         System.out.println("Hello, world!");
     }
 }
