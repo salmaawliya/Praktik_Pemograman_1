892a379e9f2e3501478a1d27412dc56e4ec2eb8f
John Doe <you@example.com>
2021-09-14 16:00:34 +0700

    mencoba python casting 2

--- Pertemuan 3/variabel.py
+++ Pertemuan 3/variabel.py
@@ -5,6 +5,11 @@ tinggiBadan = 155
 beratBadan = 45.5
 print("Nama depan saya adalah " + namaDepan)
 print("Nama Belakang saya adalah " + namaBelakang)
-print("Umur saya adalah " + str(umur) + " tahun")
+print("Umur saya adalah " + str(umur) + " tahun")               # menambahkan casting string agar teks bisa terbaca
 print("Tinggi badan saya adalah " + str(tinggiBadan) + " cm")
 print("Berat badan saya adalah " + str(beratBadan) + " kg")
+print(type(namaDepan))
+print(type(namaBelakang))
+print(type(umur))
+print(type(tinggiBadan))
+print(type(beratBadan))
