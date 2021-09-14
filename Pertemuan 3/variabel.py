2edecd379cd043efb6af36cea56fa2e61ae5eb17
John Doe <you@example.com>
2021-09-14 14:45:15 +0700

    mencoba variabel di python

---
+++ Pertemuan 3/variabel.py
@@ -0,0 +1,10 @@
+namaDepan = "Salma"
+namaBelakang = "Awliya Nafsani"
+umur = 17
+tinggiBadan = 155
+beratBadan = 45.5
+print("Nama depan saya adalah " + namaDepan)
+print("Nama Belakang saya adalah " + namaBelakang)
+print(umur)
+print(tinggiBadan)
+print(beratBadan)
