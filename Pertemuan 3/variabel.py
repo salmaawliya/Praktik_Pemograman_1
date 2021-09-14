eda8dff5824c33f6b09d2c8753d3dbc18d11209c
John Doe <you@example.com>
2021-09-14 15:53:57 +0700

    mencoba python casting

--- Pertemuan 3/variabel.py
+++ Pertemuan 3/variabel.py
@@ -5,6 +5,6 @@ tinggiBadan = 155
 beratBadan = 45.5
 print("Nama depan saya adalah " + namaDepan)
 print("Nama Belakang saya adalah " + namaBelakang)
-print(umur)
-print(tinggiBadan)
-print(beratBadan)
+print("Umur saya adalah " + str(umur) + " tahun")
+print("Tinggi badan saya adalah " + str(tinggiBadan) + " cm")
+print("Berat badan saya adalah " + str(beratBadan) + " kg")
