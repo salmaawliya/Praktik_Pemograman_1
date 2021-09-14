cc5afabca9603cd8e67abec42a730c0c08b4d3bc
John Doe <you@example.com>
2021-09-14 21:03:34 +0700

    tugas pertemuan 3

---
+++ Pertemuan 3/IdentitasDiri.java
@@ -0,0 +1,21 @@
+public class IdentitasDiri {
+    public static void main(String[] args) {
+        System.out.println("Identitas Diri Mahasiswa UNY");
+        
+        String nama = "Salma Awliya Nafsani";
+        String nim = "21537144015";
+        int angkatan = 2021;
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "Pendidikan Teknik Elektronika dan Informatika";
+        String fakultas = "Teknik";
+        double ipkHarapan = 3.55;
+
+        System.out.println("Nama            : " + nama);
+        System.out.println("NIM             : " + nim);
+        System.out.println("Angkatan        : " + angkatan);
+        System.out.println("Program Studi   : " + programStudi);
+        System.out.println("Jurusan         : " + jurusan);
+        System.out.println("Fakultas        : " + fakultas);
+        System.out.println("IPK Harapan     : Diatas " + ipkHarapan );
+    }
+}
---
+++ Pertemuan 3/identitasdiri.py
@@ -0,0 +1,18 @@
+print("Identitas Diri Mahasiswa UNY")
+
+nama = "Salma Awliya Nafsani"
+nim = "21537144015"
+angkatan = 2021
+programStudi = "Teknologi Informasi"
+jurusan = "Pendidikan Teknik Informatika dan Elektronika"
+fakultas = "Teknik"
+ipkHarapan = 3.55
+
+print("Nama             : " + nama)
+print("NIM              : " + nim)
+print("Angkatan         : " + str(angkatan))
+print("Program Studi    : " + programStudi)
+print("Jurusan          : " + jurusan)
+print("Fakultas         : " + fakultas)
+print("IPK Harapan      : " + str(ipkHarapan))
+
