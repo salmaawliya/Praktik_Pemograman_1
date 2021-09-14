e469324f565d835102c3721849f57bde2c9ac655
John Doe <you@example.com>
2021-09-14 16:08:56 +0700

    mencoba java type casting

--- Pertemuan 3/Variabel.java
+++ Pertemuan 3/Variabel.java
@@ -7,8 +7,8 @@ public class Variabel {
         double beratBadan = 45.5;
         System.out.println("Nama saya " + namaDepan);
         System.out.println("Nama belakang saya " + namaBelakang);
-        System.out.println("Umur saya " + umur);
-        System.out.println("Tinggi badan saya " + tinggiBadan);
-        System.out.println("Berat badan saya " + beratBadan);
+        System.out.println("Umur saya " + (double)umur);  //mengubah nilai int ke double
+        System.out.println("Tinggi badan saya " + (int)tinggiBadan);  //mengubah nilai double ke int
+        System.out.println("Berat badan saya " + (int)beratBadan);  //mengubah nilai double ke int
     }
 }
