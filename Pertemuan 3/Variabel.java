a708e8902358601a1551f0d07d28079806afe1f2
John Doe <you@example.com>
2021-09-14 13:55:40 +0700

    mencoba variabel di java

---
+++ Pertemuan 3/Variabel.java
@@ -0,0 +1,14 @@
+public class Variabel {
+    public static void main(String[] args) {
+        String namaDepan = "Salma";
+        String namaBelakang = "Awliya Nafsani";
+        int umur = 17;
+        double tinggiBadan = 155;
+        double beratBadan = 45.5;
+        System.out.println("Nama saya " + namaDepan);
+        System.out.println("Nama belakang saya " + namaBelakang);
+        System.out.println("Umur saya " + umur);
+        System.out.println("Tinggi badan saya " + tinggiBadan);
+        System.out.println("Berat badan saya " + beratBadan);
+    }
+}
